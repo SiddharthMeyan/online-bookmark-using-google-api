5460a4ee7998789dcb7c645f3dd75b5dec5b3ea1
SiddharthMeyan <siddharthmeyan@gmail.com>
2021-05-15 00:08:48 +0530

    table to show books

--- bookmark.js
+++ bookmark.js
@@ -11,14 +11,33 @@
 // };
 
 // ==========================================So we would use async / await==========================================
+base = `https://www.googleapis.com/books/v1/volumes?q=`;
+query = document.getElementById("search");
+bookname.addEventListener("change", (e) => {
+  searchBook = e.target.value;
+});
+
+author.addEventListener("change", (e) => (searchAuthor = e.target.value));
+
+reset = () => {
+  bookname.value = "";
+  author.value = "";
+};
+
 async function getBook() {
-  base =
-    "https://www.googleapis.com/books/v1/volumes?q=meditations+inauthor:marcus&key=AIzaSyBR9sGZNXJjnB-TZhYcXtfGsosmCAwcLYU";
-  const response = await fetch(base);
+  url = `${base}${searchBook}+inauthor:${searchAuthor}&key=AIzaSyBR9sGZNXJjnB-TZhYcXtfGsosmCAwcLYU`;
+  const response = await fetch(url);
   const result = response.json();
   console.log(result);
   return result;
 }
 
-mybook = getBook();
-mybook.then((data) => console.log(data.items[0].volumeInfo.title));
+searchBooks = () => {
+  mybook = getBook();
+  mybook.then((data) => data.items[0].volumeInfo.title);
+};
+
+displayBooks = () => {
+  myBooks = searchBooks();
+  console.log(myBooks);
+};
