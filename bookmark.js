9ee2fd4dea63f43b49a1ca7b7bb98b7e2f9607c2
SiddharthMeyan <75212515+SiddharthMeyan@users.noreply.github.com>
2021-05-15 14:14:14 +0530

    Add files via upload

--- bookmark.js
+++ bookmark.js
@@ -1,4 +1,4 @@
-// ==========================================Fetch API isnt working for somereason==========================================
+// ==========================================Fetch API isnt working for some reason==========================================
 // getBook = () => {
 //   base =
 //     "https://www.googleapis.com/books/v1/volumes?q=meditations+inauthor:marcus&key=AIzaSyBR9sGZNXJjnB-TZhYcXtfGsosmCAwcLYU";
@@ -12,6 +12,7 @@
 
 // ==========================================So we would use async / await==========================================
 base = `https://www.googleapis.com/books/v1/volumes?q=`;
+showRes = document.getElementById("showRes");
 query = document.getElementById("search");
 bookname.addEventListener("change", (e) => {
   searchBook = e.target.value;
@@ -34,10 +35,42 @@ async function getBook() {
 
 searchBooks = () => {
   mybook = getBook();
-  mybook.then((data) => data.items[0].volumeInfo.title);
+  mybook.then((data) => {
+    a = data.items[0].volumeInfo.title;
+    desc = data.items[0].volumeInfo.description;
+    authors = data.items[0].volumeInfo.authors;
+    pageCount = data.items[0].volumeInfo.pageCount;
+    showRes.innerHTML += `<tr>
+    <td>${a}</td>
+    <td>${authors}</td>
+    <td> <input onblur="changeMe(value)" contenteditable="true" class="col-md-7" id="pagesTag${a}" type="number" > of ${pageCount}</td>
+    <td>
+    <a type="button" class="btn btn-link" onclick="showPopup()" data-bs-toggle="modal" data-bs-target="#exampleModal${a}">
+    Description
+  `;
+    showPopup = () => {
+      alert(desc);
+    };
+    a.value = "";
+    desc.value = "";
+    authors.value = "";
+    pageCount.value = "";
+    changeMe = (value) => {
+      pagesTag = document.getElementById(`pagesTag${a}`);
+      pagesTag.value = value;
+    };
+    // pagesTags.innerText += `<p>THIS</p>`;
+    // {
+    //   pagesTags = document.getElementById(`pagesTag${a}`);
+    //   pagesTags.addEventListener("blur", (e) => {
+    //     let my_div_text = pagesTags.innerText;
+    //     pagesTags.innerHTML = `<p>${my_div_text}</p>`;
+    //   });
+    // }
+  });
 };
 
-displayBooks = () => {
-  myBooks = searchBooks();
-  console.log(myBooks);
-};
+// getPages = (e) => {
+//   myVal = e.target.value;
+//   return myVal;
+// };
